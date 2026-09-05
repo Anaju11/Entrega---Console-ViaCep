1490873ddc72fe6443404360b245fef6e8f8205b
AnaJuBrum <v.anajuliabrum@gmail.com>
2026-09-05 15:27:26 -0300

    envio da atividade.

---
+++ Endereco.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CostumerViaCep.Models
+{
+    public class Endereco
+    {
+    
+            public string? Cep { get; set; }
+            public string? Logradouro { get; set; }
+            public string? Unidade {get;set;}
+            public string? Bairro { get; set; }
+            public string?  Localidade { get; set; }
+            public string? Uf { get; set; }
+            public string? Ibge { get; set; }
+            public string? Gia { get; set; }
+            public string? DDD { get; set; }
+            public string? Siafi { get; set; }
+            
+
+    }
+}
